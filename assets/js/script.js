458259b2d4044ba0555d8724cf9ee8838dd326bc
Kendrameda <gibsoken@gmail.com>
2022-12-09 15:12:20 -0600

    connected cities to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,46 @@
 
 
-let APIKey = 'beda6b198599b8d54c518efb4033e99f';
+let APIKey = '367a5f4cd9c1e6933757256785456545';
 
-var ctiy_name;
+var lang = 'en';
+var units = 'imperial';
 
 function displayCity() {
 
-    var API_Url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+    var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
 
     fetch(API_Url)
     .then(function (response) {
       return response.json();
     })
       .then(function (data) {
-        console.log(data)
+        console.log(data);
+        console.log('test');
 
+        $(".current-temp").empty();
+
+        var featureCard = $("<div>");
+
+        var city = data.city.name;
+
+
+        var realTemp = data.list[0].main.temp;
+        console.log(realTemp);
+
+        var windSpeed = data.list[0].wind.speed;
+
+        var windDirect = data.list[0].wind.deg;
+
+        var humid = data.list[0].main.humidity;
+
+        featureCard.append("<br/>" + "Current city: " + city  + "<br/>");
+
+        featureCard.append("<br/>" + "Current Temp: " + realTemp + '℉' + "<br/>");
+        
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + ' ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
+
+        $(".current-temp").append(featureCard);
 
       });
 
@@ -24,9 +50,79 @@ $("#atlanta").on("click", function (event){
     event.preventDefault();
     console.log('testing 123');
     
-    lat =
-    lon = 
+   city = 'atlanta';
 
     displayCity();
 
 });
+
+$("#denver").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'Denver';
+
+    displayCity();
+
+});
+
+$("#seatle").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'Seattle';
+
+    displayCity();
+
+});
+
+$("#sanFran").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'San Fransisco';
+
+    displayCity();
+
+});
+
+$("#orlando").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'Orlando';
+
+    displayCity();
+
+});
+
+$("#NY").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'New York';
+
+    displayCity();
+
+});
+
+$("#chicago").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'Chicago';
+
+    displayCity();
+
+});
+
+$("#austic").on("click", function (event){
+    event.preventDefault();
+    console.log('testing 123');
+    
+   city = 'Austin';
+
+    displayCity();
+
+});
+
