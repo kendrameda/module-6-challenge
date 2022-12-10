ac4eac03c571eb3a951eff5e3562f6d75b44b6e1
Kendra Gibson <kendragibson@Kendras-MacBook-Pro.local>
2022-12-09 19:16:06 -0600

    final touches without local storage yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@ let APIKey = '367a5f4cd9c1e6933757256785456545';
 
 var lang = 'en';
 var units = 'imperial';
+var city = $("#cname").val().trim();
 
 function displayCity() {
 
+  var city = $("#cname").val().trim();
     var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
 
     fetch(API_Url)
@@ -17,7 +19,7 @@ function displayCity() {
         console.log(data);
         console.log('test');
 
-        $(".current-temp").empty();
+        $("#current-temp").empty();
 
         var featureCard = $("<div>");
 
@@ -37,10 +39,10 @@ function displayCity() {
 
         featureCard.append("<br/>" + "Current Temp: " + realTemp + '℉' + "<br/>");
         
-        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + ' ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + 'mph ' + windDirect + '°' + "<br/>");
         featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
 
-        $(".current-temp").append(featureCard);
+        $("#current-temp").append(featureCard);
 
         
 
@@ -48,7 +50,10 @@ function displayCity() {
 
 };
 
-function fiveDay( ) {
+
+function tomorrow() {
+
+  var city = $("#cname").val().trim();
     var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
 
     fetch(API_Url)
@@ -58,132 +63,290 @@ function fiveDay( ) {
       .then(function (data) {
         console.log(data);
         console.log('test');
-      })
 
-      var realTemp = data.list[1].main.temp;
-        console.log(realTemp);
-
-        var windSpeed = data.list[1].wind.speed;
-
-        var windDirect = data.list[1].wind.deg;
-
-        var humid = data.list[1].main.humidity;
-
-        $(".fiveDay").empty();
+        $(".tomorrow").empty();
 
         var featureCard = $("<div>");
 
         var city = data.city.name;
 
 
-        var realTemp = data.list[0].main.temp;
+        var realTemp = data.list[7].main.temp;
         console.log(realTemp);
 
-        var windSpeed = data.list[0].wind.speed;
+        var windSpeed = data.list[7].wind.speed;
 
-        var windDirect = data.list[0].wind.deg;
+        var windDirect = data.list[7].wind.deg;
 
-        var humid = data.list[0].main.humidity;
+        var humid = data.list[7].main.humidity;
 
-        featureCard.append("<br/>" + "Current city: " + city  + "<br/>");
+        featureCard.append("<br/>" + "Selected city: " + city  + "<br/>");
 
-        featureCard.append("<br/>" + "Current Temp: " + realTemp + '℉' + "<br/>");
+        featureCard.append("<br/>" + "Predicted Temp: " + realTemp + '℉' + "<br/>");
         
-        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + ' ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + 'mph ' + windDirect + '°' + "<br/>");
         featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
 
-        $(".fiveDay").append(featureCard);
-}
+        $(".tomorrow").append(featureCard);
 
-$("#submit").on('click', function(event){
-  event.preventDefault();
-    console.log('testing 123');
-    
-   city  = "#cityName";
+        
 
-    displayCity();
-    fiveDay();
-}
-)
+      });
 
-$("#atlanta").on("click", function (event){
+};
+
+function dayAfter() {
+
+  var city = $("#cname").val().trim();
+    var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
+
+    fetch(API_Url)
+    .then(function (response) {
+      return response.json();
+    })
+      .then(function (data) {
+        console.log(data);
+        console.log('test');
+
+        $(".dayafter").empty();
+
+        var featureCard = $("<div>");
+
+        var city = data.city.name;
+
+
+        var realTemp = data.list[18].main.temp;
+        console.log(realTemp);
+
+        var windSpeed = data.list[18].wind.speed;
+
+        var windDirect = data.list[18].wind.deg;
+
+        var humid = data.list[18].main.humidity;
+
+        featureCard.append("<br/>" + "Selected city: " + city  + "<br/>");
+
+        featureCard.append("<br/>" + "Predicted Temp: " + realTemp + '℉' + "<br/>");
+        
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + 'mph ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
+
+        $(".dayafter").append(featureCard);
+
+        
+
+      });
+
+};
+
+function dayAfter3() {
+
+  var city = $("#cname").val().trim();
+    var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
+
+    fetch(API_Url)
+    .then(function (response) {
+      return response.json();
+    })
+      .then(function (data) {
+        console.log(data);
+        console.log('test');
+
+        $(".card-text3").empty();
+
+        var featureCard = $("<div>");
+
+        var city = data.city.name;
+
+
+        var realTemp = data.list[27].main.temp;
+        console.log(realTemp);
+
+        var windSpeed = data.list[27].wind.speed;
+
+        var windDirect = data.list[27].wind.deg;
+
+        var humid = data.list[27].main.humidity;
+
+        featureCard.append("<br/>" + "Selected city: " + city  + "<br/>");
+
+        featureCard.append("<br/>" + "Predicted Temp: " + realTemp + '℉' + "<br/>");
+        
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + 'mph ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
+
+        $(".card-text3").append(featureCard);
+
+        
+
+      });
+
+};
+
+function dayAfter4() {
+
+  var city = $("#cname").val().trim();
+    var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
+
+    fetch(API_Url)
+    .then(function (response) {
+      return response.json();
+    })
+      .then(function (data) {
+        console.log(data);
+        console.log('test');
+
+        $(".card-text4").empty();
+
+        var featureCard = $("<div>");
+
+        var city = data.city.name;
+
+
+        var realTemp = data.list[35].main.temp;
+        console.log(realTemp);
+
+        var windSpeed = data.list[35].wind.speed;
+
+        var windDirect = data.list[35].wind.deg;
+
+        var humid = data.list[35].main.humidity;
+
+        featureCard.append("<br/>" + "Selected city: " + city  + "<br/>");
+
+        featureCard.append("<br/>" + "Predicted Temp: " + realTemp + '℉' + "<br/>");
+        
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + 'mph ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
+
+        $(".card-text4").append(featureCard);
+
+        
+
+      });
+
+};
+
+function dayAfter5() {
+
+  var city = $("#cname").val().trim();
+    var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
+
+    fetch(API_Url)
+    .then(function (response) {
+      return response.json();
+    })
+      .then(function (data) {
+        console.log(data);
+        console.log('test');
+
+        $(".card-text5").empty();
+
+        var featureCard = $("<div>");
+
+        var city = data.city.name;
+
+
+        var realTemp = data.list[39].main.temp;
+        console.log(realTemp);
+
+        var windSpeed = data.list[39].wind.speed;
+
+        var windDirect = data.list[39].wind.deg;
+
+        var humid = data.list[39].main.humidity;
+
+        featureCard.append("<br/>" + "Selected city: " + city  + "<br/>");
+
+        featureCard.append("<br/>" + "Predicted Temp: " + realTemp + '℉' + "<br/>");
+        
+        featureCard.append("<br/>" + "Wind Speed: " + windSpeed + 'mph ' + windDirect + '°' + "<br/>");
+        featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
+
+        $(".card-text5").append(featureCard);
+
+        
+
+      });
+
+};
+
+$("#cname").on('click', function (event){
     event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'atlanta';
+    console.log('button was clicked?!?');
 
     displayCity();
-    fiveDay();
+    tomorrow();
+    dayAfter();
+dayAfter3();
+dayAfter4();
+dayAfter5();
 
-});
 
-$("#denver").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'Denver';
 
-    displayCity();
 
-});
+  })
 
-$("#seatle").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'Seattle';
-
-    displayCity();
-
-});
-
-$("#sanFran").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'San Fransisco';
-
-    displayCity();
-
-});
-
-$("#orlando").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'Orlando';
-
-    displayCity();
-
-});
-
-$("#NY").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'New York';
-
-    displayCity();
-
-});
-
-$("#chicago").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'Chicago';
-
-    displayCity();
-
-});
-
-$("#austic").on("click", function (event){
-    event.preventDefault();
-    console.log('testing 123');
-    
-   city = 'Austin';
-
-    displayCity();
-
-});
-
+  
+  
+  // $(document).ready(function() {
+  //   M.updateTextFields();
+  // });
+  
+  // function fiveDay( ) {
+  //     var API_Url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=${units}&lang=${lang}`;
+  
+  //     fetch(API_Url)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //       .then(function (data) {
+  //         console.log(data);
+  //         console.log('test');
+  //       })
+  
+  //       var realTemp = data.list[1].main.temp;
+  //         console.log(realTemp);
+  
+  //         var windSpeed = data.list[1].wind.speed;
+  
+  //         var windDirect = data.list[1].wind.deg;
+  
+  //         var humid = data.list[1].main.humidity;
+  
+  //         $(".fiveDay").empty();
+  
+  //         var featureCard = $("<div>");
+  
+  //         var city = data.city.name;
+  
+  
+  //         var realTemp = data.list[0].main.temp;
+  //         console.log(realTemp);
+  
+  //         var windSpeed = data.list[0].wind.speed;
+  
+  //         var windDirect = data.list[0].wind.deg;
+  
+  //         var humid = data.list[0].main.humidity;
+  
+  //         featureCard.append("<br/>" + "Current city: " + city  + "<br/>");
+  
+  //         featureCard.append("<br/>" + "Current Temp: " + realTemp + '℉' + "<br/>");
+          
+  //         featureCard.append("<br/>" + "Wind Speed: " + windSpeed + ' ' + windDirect + '°' + "<br/>");
+  //         featureCard.append("<br/>" + "Humidity: " + humid + '%' + "<br/>");
+  
+  //         $(".fiveDay").append(featureCard);
+  // }
+  
+  // $("#submit").on('click', function(event){
+  //   event.preventDefault();
+  //     console.log('testing 123');
+      
+  //    city  = "#cityName";
+  
+  //     displayCity();
+  //     fiveDay();
+  // }
+  // )
